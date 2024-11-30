0c5c4e6ea729dcaf598e1f7d73191853252c8343
Michelle <micastrey@gmail.com>
2024-11-30 14:11:08 -0300

    Ads

---
+++ mapaLinguagem.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+struct Candidato{
+    int numero;
+    char nome[50];
+    int votos;
+};
+int votar(struct Candidato candidatos[], int totalCandidatos) {
+    int voto;
+    printf("Digite o número do candidato (1 a 99): ");
+    scanf("%d", &voto);
+    for (int i = 0; i < totalCandidatos; i++){
+        if (candidatos[i].numero == voto){
+            candidatos[i].votos++;
+            
+            printf("Voto computado para %s!\n", candidatos[i].nome);
+            return 1;
+        }
+   
+  }
+  printf("Número  de candidato inválido");
+    return 0;
+}
+
+void apurarVotos(struct Candidato candidatos[], int totalCandidatos ){
+    printf("\nResultado da apuração de votos:\n");
+    for (int i = 0; i < totalCandidatos; i++) {
+        printf("%s (Número %d): %d votos \n", candidatos[i].nome, candidatos[i].numero, candidatos[i].votos);
+    }
+}
+void percentualVotos(struct Candidato candidatos[], int totalCandidatos, int totalVotos){
+    if (totalVotos == 0){
+        printf("Nenhum voto computado ainda.\n");
+        return;
+    }
+    printf("\nPercentual de votos: \n");
+    for (int i = 0; i < totalCandidatos; i++){
+        float percentual = (candidatos[i].votos * 100.0) / totalVotos;
+        printf("%s: %.2f%% dos votos\n", candidatos[i].nome, percentual);
+    }
+}
+
+int main() {
+    struct Candidato candidatos[3] = {{10, "João da Padaria", 0}, 
+    {20, "Maria da Farmácia", 0}, 
+    {30, "Zé do Gás", 0}
+    };
+    int opcao;
+    int totalVotos = 0;
+    int totalCandidatos = 3;
+
+    do {
+        printf("\n-----Menu de opções:-----\n");
+        printf("1. Votar\n");
+        printf("2. Apurar\n");
+        printf("3. Emitir percentual de votos\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opção: \n");
+        scanf("%d", &opcao);
+        switch (opcao) {
+             case 1: {
+            totalVotos += votar(candidatos, totalCandidatos);
+            break;
+             }
+             case 2: {
+            apurarVotos(candidatos, totalCandidatos);
+            break;
+             }
+             case 3: {
+            percentualVotos(candidatos, totalCandidatos, totalVotos);
+            break;
+             }
+             case 4: {
+            printf("Saindo...\n");
+            break;
+             }
+            default: {
+            printf("Opção inválida!\n");
+            break;
+            }
+        }
+    }   while (opcao != 4);
+
+    return 0;
+}
